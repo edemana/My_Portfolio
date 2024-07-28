e938e0f0f901a7c91db962e312db798b52bea1db
DevEdemana <anagbahedem@gmail.com>
2024-07-28 16:54:48 +0000

    update

--- src/images/index.js
+++ src/images/index.js
@@ -1,5 +1,5 @@
 export {default as profile1} from "./profile-1.png"
-export {default as profile2} from "./profile-2.jpg"
+export {default as profile2} from "./profile-2.png"
 export {default as profile3} from "./profile-3.jpeg"
 export {default as profile4} from "./profile-4.jpeg"
 export {default as profile5} from "./profile-5.jpeg"
